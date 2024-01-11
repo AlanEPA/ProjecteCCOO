89214c2572a2618eeab294f29a6975aed2ed755e
Alan <alan.roura.7e6@itb.cat>
2024-01-11 13:42:17 +0100

    Landing page

---
+++ Assets/Scripts/ManagerScene.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+public class ManagerScene : MonoBehaviour
+{
+    public GameObject canvasActual;
+    public GameObject canvasSiguiente;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ResetButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void CambiarCanvas()
+    {
+        canvasActual.SetActive(false);
+        canvasSiguiente.SetActive(true);
+    }
+
+}
