dcc9ecc8dbf58442967104cea5c0f7e748954cbe
Alan <alan.roura.7e6@itb.cat>
2024-01-12 10:06:46 +0100

    MockupS14 V1

--- Assets/Scripts/Settings2.cs
+++ Assets/Scripts/Settings2.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class ChangeSceneToTutorial : MonoBehaviour
+
+public class Settings2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -15,8 +16,9 @@ public class ChangeSceneToTutorial : MonoBehaviour
     {
         
     }
+
     public void ChangeScene()
     {
-        SceneManager.LoadScene("Tutorial1");
+        SceneManager.LoadScene("Settings 2");
     }
 }
